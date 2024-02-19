080ec96fc6228fec96d32125219d64dbda001ba6
Ale021217 <110646535+Ale021217@users.noreply.github.com>
2024-02-19 12:05:28 -0600

    about2

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
+
+
 import React from "react";
 import { View } from "react-native";
 import Index from "./src/views";
+import AboutPage from "./src/views/AboutPage";
 
 const App=()=>{
   return (
     <View>
       <Index/>
+      <AboutPage/>
     </View>
   );
 };
